807b298899921cd3a0790c1b746b3c4714adbab7
Alex Ryan <87581318+alexRyan719@users.noreply.github.com>
2024-10-06 18:39:29 -0600

    Update test.py

--- python-project/test.py
+++ python-project/test.py
@@ -2,14 +2,18 @@ import random
 
 print("Hello World")
 
+
+# range of values for lowercase: 97-122
+# range of values for uppercase: 65-90
 def gen_first_name(length):
   name = ""
+  print(length)
   for letter in range(length):
-    name += 'a'
+    name += char(random.randrange(97,122))
   return name
 
 
 
 #def create_user_hash(f_name, l_name):
 
-print(gen_first_name(random.randrange(5)))
+print(gen_first_name(random.randrange(20)))
