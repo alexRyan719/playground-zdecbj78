df9880213795c574068deaee309cf64ccea89e8b
Alex Ryan <87581318+alexRyan719@users.noreply.github.com>
2024-10-06 18:15:34 -0600

    Create test.py

---
+++ python-project/test.py
@@ -0,0 +1,13 @@
+print("Hello World")
+
+def gen_first_name(length):
+  name = ""
+  for letter in range(length):
+    name += 'a'
+  return name
+
+
+
+#def create_user_hash(f_name, l_name):
+
+print(gen_first_name(randrange(5))
