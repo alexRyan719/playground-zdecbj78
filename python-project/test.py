e98f6ea7867f86dac78aa63570f834d068ff2a3c
Alex Ryan <87581318+alexRyan719@users.noreply.github.com>
2025-03-05 19:07:54 -0700

    Update test.py update hash variables and output
    Updated the "hash" variable to be 3 different variables and updated the corresponding function calls. Also cloned and updated the output section to reflect the three different possible hashes for the voter information.

--- python-project/test.py
+++ python-project/test.py
@@ -56,13 +56,13 @@ first_name = gen_first_name(random.randrange(3,10))
 last_name = gen_last_name(random.randrange(4,10))
 
 # Weak hash, MD5
-hash = gen_user_hash_md5(first_name, last_name)
+hash_md5 = gen_user_hash_md5(first_name, last_name)
 
 # Stronger hash, SHA256
-hash = gen_user_hash_sha256(first_name, last_name)
+hash_sha256 = gen_user_hash_sha256(first_name, last_name)
 
 # Even Stronger hash but potential performance issues, SHA512
-hash = gen_user_hash_sha512(first_name, last_name)
+hash_sha512 = gen_user_hash_sha512(first_name, last_name)
 
 print("First Name: ")
 print(first_name)
@@ -72,8 +72,16 @@ print("Last Name: ")
 print(last_name)
 print()
 
-print("Hash: ")
-print(hash.hexdigest())
+print("MD5 Hash : ")
+print(hash_md5.hexdigest())
+print()
+
+print("SHA256 Hash: ")
+print(hash_sha256.hexdigest())
+print()
+
+print("SHA512 Hash: ")
+print(hash_sha512.hexdigest())
 print()
 
 print("US States: ")
